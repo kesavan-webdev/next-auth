7822597fb4d35c923f55037753a830e74668f9b5
kesavan <kesavan@mericaninc.com>
2023-06-21 14:48:49 +0530

    create navbar and install react-icons

---
+++ components/header/header.component.jsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import { TfiWrite } from "react-icons/tfi";
+import { GoSignIn } from "react-icons/go";
+
+const Header = () => {
+  return (
+    <nav className="navbar bg-slate-500 flex justify-between p-5 items-center h-16">
+      <div>
+        <Image src={"/logo.png"} alt="logo" width={180} height={40} />
+      </div>
+      <div className="flex gap-5">
+        <button className="bg-white rounded-full shadow-lg p-2 hover:bg-slate-400 hover:text-white">
+          <span className="hidden md:block">Create Post</span>{" "}
+          <TfiWrite className="md:hidden px-2 w-full h-full" />
+        </button>
+        <button className="bg-white rounded-full shadow-lg p-2 hover:bg-slate-400 hover:text-white">
+          <span className="hidden md:block"> Sign In</span>
+          <GoSignIn className="md:hidden px-2 w-full h-full" />
+        </button>
+        <Image src={"profile.svg"} alt="logo" width={40} height={40} />
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
