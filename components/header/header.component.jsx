52072dd44c4b56035cdac8df69b022ea5a451352
kesavan <kesavan@mericaninc.com>
2023-06-21 17:33:12 +0530

    add google auth using next/auth

--- components/header/header.component.jsx
+++ components/header/header.component.jsx
@@ -1,8 +1,10 @@
+"use client";
 import Image from "next/image";
 import { TfiWrite } from "react-icons/tfi";
-import { GoSignIn } from "react-icons/go";
-
+import { GoSignIn, GoSignOut } from "react-icons/go";
+import { signIn, signOut, useSession } from "next-auth/react";
 const Header = () => {
+  const { data: session } = useSession();
   return (
     <nav className="navbar bg-slate-500 flex justify-between p-5 px-2 items-center h-16">
       <div>
@@ -10,13 +12,28 @@ const Header = () => {
       </div>
       <div className="flex gap-5">
         <button className="bg-white rounded-full shadow-lg p-2 hover:bg-slate-400 hover:text-white">
-          <span className="hidden md:block">Create Post</span>{" "}
+          <span className="hidden md:block">Create Post</span>
           <TfiWrite className="md:hidden px-2 w-full h-full" />
         </button>
-        <button className="bg-white rounded-full shadow-lg p-2 hover:bg-slate-400 hover:text-white">
-          <span className="hidden md:block"> Sign In</span>
-          <GoSignIn className="md:hidden px-2 w-full h-full" />
-        </button>
+
+        {session && session.user ? (
+          <button
+            className="bg-white rounded-full shadow-lg p-2 hover:bg-slate-400 hover:text-white"
+            onClick={() => signOut()}
+          >
+            <span className="hidden md:block"> Sign Out</span>
+            <GoSignOut className="md:hidden px-2 w-full h-full" />
+          </button>
+        ) : (
+          <button
+            className="bg-white rounded-full shadow-lg p-2 hover:bg-slate-400 hover:text-white"
+            onClick={() => signIn()}
+          >
+            <span className="hidden md:block"> Sign In</span>
+            <GoSignIn className="md:hidden px-2 w-full h-full" />
+          </button>
+        )}
+
         <Image
           className="cursor-pointer"
           src={"profile.svg"}
@@ -25,6 +42,7 @@ const Header = () => {
           height={40}
         />
       </div>
+      {console.log(session)}
     </nav>
   );
 };
