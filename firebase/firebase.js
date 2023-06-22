57bd0387a3a7d56290635d16700745b76e66c629
kesavan <kesavan@mericaninc.com>
2023-06-22 16:37:04 +0530

    initialize firebase

---
+++ firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAJ0MXDoMGoKG_02Dej3d5s9T_m2-GnMek",
+  authDomain: "arun-softsolutions.firebaseapp.com",
+  projectId: "arun-softsolutions",
+  storageBucket: "arun-softsolutions.appspot.com",
+  messagingSenderId: "264181169039",
+  appId: "1:264181169039:web:95f7f287bb3e50fad969fa",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
