6d2f5aa4f52b0d52f391f135b7295dc57d1a0287
kesavan <kesavan@mericaninc.com>
2023-06-22 16:32:39 +0530

    install firebase

--- components/gamelist/gamelist.component.jsx
+++ components/gamelist/gamelist.component.jsx
@@ -1,25 +1,25 @@
+"use client";
+import { useEffect, useState } from "react";
 import GameListData from "./games/data";
 import Image from "next/image";
 
 const GameList = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    setData(GameListData);
+  }, []);
   return (
-    <div className="flex items-center justify-center gap-8 mt-5">
-      {GameListData.map((val) => {
+    <div className="flex items-center justify-center gap-8 mt-5 px-5">
+      {data.map((val) => {
         return (
           <div
             key={val.id}
             className="flex flex-col items-center justify-center w-20 h-20 "
           >
-            <div className="w-full h-full flex justify-center items-center">
-              <Image
-                className="hover:animate-bounce transition duration-700 ease-out"
-                src={val.image}
-                width={40}
-                height={40}
-                alt={val.name}
-              />
+            <div className="w-full h-full flex justify-center items-center hover:animate-bounce transition duration-700 ease-out">
+              <Image src={val.image} width={40} height={40} alt={val.name} />
             </div>
-            <span className="">{val.name}</span>
+            <small className="font-semibold">{val.name}</small>
           </div>
         );
       })}
