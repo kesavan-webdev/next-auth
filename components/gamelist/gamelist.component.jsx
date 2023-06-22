e3d53617a6ec9328c577d8c77550c5c8cc610637
kesavan <kesavan@mericaninc.com>
2023-06-22 15:27:41 +0530

    add gamelist

--- components/gamelist/gamelist.component.jsx
+++ components/gamelist/gamelist.component.jsx
@@ -1,5 +1,29 @@
-const GameList = () => {
-  return <div>GameList</div>;
-};
+import GameListData from "./games/data";
+import Image from "next/image";
 
+const GameList = () => {
+  return (
+    <div className="flex items-center justify-center gap-8 mt-5">
+      {GameListData.map((val) => {
+        return (
+          <div
+            key={val.id}
+            className="flex flex-col items-center justify-center w-20 h-20 "
+          >
+            <div className="w-full h-full flex justify-center items-center">
+              <Image
+                className="hover:animate-bounce transition duration-700 ease-out"
+                src={val.image}
+                width={40}
+                height={40}
+                alt={val.name}
+              />
+            </div>
+            <span className="">{val.name}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 export default GameList;
---
+++ components/gamelist/games/data.js
@@ -0,0 +1,29 @@
+const GameListData = [
+  {
+    id: 1,
+    name: "Cricket",
+    image: "/cricket.png",
+  },
+  {
+    id: 2,
+    name: "Tennis",
+    image: "/tennis.png",
+  },
+  {
+    id: 3,
+    name: "Volleyball",
+    image: "/volleyball.png",
+  },
+  {
+    id: 4,
+    name: "Football",
+    image: "/football.jpg",
+  },
+  {
+    id: 5,
+    name: "OtherGames",
+    image: "/dice.png",
+  },
+];
+
+export default GameListData;
